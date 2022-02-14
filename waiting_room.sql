3e90ccd363f09b37e9b4f6877e8bcbdf13a16636
teodorask <tedy19995@gmail.com>
2022-02-14 02:25:59 +0200

    test data

--- waiting_room.sql
+++ waiting_room.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 13, 2022 at 04:17 PM
+-- Generation Time: Feb 14, 2022 at 01:25 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -37,6 +37,14 @@ CREATE TABLE `messages` (
   `time` datetime NOT NULL DEFAULT '1970-01-01 00:00:01' ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`msg_id`, `room_id`, `text`, `author_id`, `author_name`, `send_to`, `time`) VALUES
+(6, 82, 'Съобщение до всички!', 6, 'Студент Едно', 0, '2022-02-13 07:34:46'),
+(7, 82, 'Съобщение до преподавател', 7, 'Студент Две', 1, '2022-02-13 07:35:21');
+
 -- --------------------------------------------------------
 
 --
@@ -62,12 +70,8 @@ CREATE TABLE `rooms` (
 --
 
 INSERT INTO `rooms` (`room_id`, `creator_id`, `moderator_id`, `name`, `description`, `type_id`, `url`, `meeting_password`, `break_until`, `avg_time`, `passed_people`) VALUES
-(1, 1, 1, 'Защита на проекти 1-ва група', 'ееееееееее\r\nееееееееее\r\nееееееееее', NULL, '', '', NULL, NULL, 0),
-(2, 1, 1, 'Устен изпит Кн2', 'е', NULL, '', '', NULL, NULL, 0),
-(3, 1, 1, 'Писмен изпит ИС', '', NULL, '', '', NULL, NULL, 0),
-(7, 5, 5, 'Стая 1', 'Описание', NULL, 'link', 'password', NULL, NULL, 0),
-(8, 5, 5, 'Test staq', 'op', NULL, 'link', 'passw', NULL, NULL, 0),
-(40, 5, 5, 'Стая', 'Стая', NULL, '', '', NULL, NULL, 0);
+(82, 21, 6, 'Защита на проект', 'Стая създадена от преподавател 1; Модератор - Студент Едно; Списък - fn2.csv; Тип - Защита на проект (~ 15 минути);', 1, 'https://bbb.fmi.uni-sofia.bg/html5client/join?sessionToken=oji2e4291n69zzmm', '', NULL, 15, 0),
+(84, 20, 20, 'Устен изпит', 'Стая създадена от преподавател 2;  създадена без списък и без тип', NULL, '', '', NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -93,21 +97,20 @@ CREATE TABLE `room_student` (
 --
 
 INSERT INTO `room_student` (`id`, `room_id`, `student_id`, `time`, `in_time`, `out_time`, `waiting`, `team`, `in_room`, `is_next`) VALUES
-(9, 40, 6, '2022-01-28 12:45:00', '2022-02-11 20:54:38', NULL, 0, 1, 0, 1),
-(10, 40, 7, '2022-02-11 03:13:08', '2022-02-11 03:13:10', NULL, 0, 2, 1, 0),
-(11, 40, 8, '2022-02-11 03:13:10', '2022-02-11 15:13:57', NULL, 0, 3, 1, 0),
-(12, 40, 9, '2022-02-11 03:13:57', '2022-02-11 15:14:11', NULL, 0, 4, 1, 0),
-(13, 40, 10, '2022-02-11 03:14:11', '2022-02-11 15:14:14', NULL, 0, 5, 1, 0),
-(14, 40, 11, '2022-02-11 03:14:14', '2022-02-11 15:14:14', NULL, 0, 6, 1, 1),
-(15, 40, 12, '2022-02-03 12:45:00', NULL, NULL, 1, 7, 1, 0),
-(16, 40, 13, '2022-02-04 12:45:00', NULL, NULL, 1, 8, 0, 0),
-(17, 40, 14, '2022-02-05 12:45:00', NULL, NULL, 1, 9, 1, 0),
-(18, 40, 15, '2022-02-06 12:45:00', NULL, NULL, 1, 10, 0, 0),
-(19, 40, 16, '2022-02-07 12:45:00', NULL, NULL, 1, 11, 0, 0),
-(20, 40, 17, '2022-02-08 12:45:00', '2022-02-11 03:13:08', NULL, 1, 12, 0, 0),
-(21, 40, 18, '2022-02-09 12:45:00', NULL, NULL, 1, 13, 0, 0),
-(22, 40, 19, '2022-02-10 12:45:00', NULL, NULL, 1, 14, 0, 0),
-(23, 2, 7, NULL, NULL, NULL, 1, 1, 0, 1);
+(69, 82, 6, '2022-01-28 12:45:00', '2022-02-13 19:33:02', NULL, 0, 1, 1, 1),
+(70, 82, 7, '2022-01-29 12:45:00', NULL, NULL, 1, 2, 0, 1),
+(71, 82, 8, '2022-01-30 12:45:00', NULL, NULL, 1, 2, 0, 1),
+(72, 82, 9, '2022-01-31 12:45:00', NULL, NULL, 1, 3, 0, 0),
+(73, 82, 10, '2022-02-01 12:45:00', NULL, NULL, 1, 3, 0, 0),
+(74, 82, 11, '2022-02-02 12:45:00', NULL, NULL, 1, 4, 0, 0),
+(75, 82, 12, '2022-02-03 12:45:00', NULL, NULL, 1, 5, 0, 0),
+(76, 82, 13, '2022-02-04 12:45:00', NULL, NULL, 1, 6, 0, 0),
+(77, 82, 14, '2022-02-05 12:45:00', NULL, NULL, 1, 7, 0, 0),
+(78, 82, 15, '2022-02-06 12:45:00', NULL, NULL, 1, 8, 0, 0),
+(79, 82, 16, '2022-02-07 12:45:00', NULL, NULL, 1, 9, 0, 0),
+(80, 82, 17, '2022-02-08 12:45:00', NULL, NULL, 1, 10, 0, 0),
+(81, 82, 18, '2022-02-09 12:45:00', NULL, NULL, 1, 11, 0, 0),
+(82, 82, 19, '2022-02-10 12:45:00', NULL, NULL, 1, 12, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -128,8 +131,7 @@ CREATE TABLE `room_type` (
 INSERT INTO `room_type` (`id`, `name`, `avg_time`) VALUES
 (1, 'Защита на проект', 15),
 (5, 'Представяне на реферат', 8),
-(6, 'Нанасяне на оценка', 5),
-(7, 'test', 5);
+(6, 'Нанасяне на оценка', 5);
 
 -- --------------------------------------------------------
 
@@ -151,23 +153,22 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `faculty_number`, `name`, `role`, `username`, `password`) VALUES
-(1, 89999, 'Петър Петров', 1, '', 'passwd'),
-(4, 81761, 'milena hr', 1, 'milena', '$2y$10$t/FfsQxbNquPBYGbsG5NLOH3f56s4W9HChLklF5W91.92Y2MYirte'),
-(5, 0, 'Милен Петров', 2, 'teacher', '$2y$10$lyIYNsp6TJhW5neRwRkt0.T4sZhdxFCLHStgOY0HWxjFkRqd/qLoy'),
-(6, 81111, 'Студент Едно', 1, 'student1', '$2y$10$S1xKJO7bGSlU8RQjjBmt2ex4Rme9ba41HcvNwnbPPoLBcaiky2wEy'),
-(7, 81222, 'Студент Две', 1, 'student2', '$2y$10$2BdT28fJmqcDDUvxkF9XHewBeyfz6Q.YrU2aNOboR5skYoWfp33fS'),
-(8, 81333, 'Студент Три', 1, 'student3', '$2y$10$kJqWLlsaPPFlVga1kU3NKefj2u8Pf5HLHB2Qnm8LGMBQuWvZFSC2u'),
+(6, 81111, 'Студент 1', 1, 'student1', '$2y$10$S1xKJO7bGSlU8RQjjBmt2ex4Rme9ba41HcvNwnbPPoLBcaiky2wEy'),
+(7, 81222, 'Студент 2', 1, 'student2', '$2y$10$2BdT28fJmqcDDUvxkF9XHewBeyfz6Q.YrU2aNOboR5skYoWfp33fS'),
+(8, 81333, 'Студент 3', 1, 'student3', '$2y$10$kJqWLlsaPPFlVga1kU3NKefj2u8Pf5HLHB2Qnm8LGMBQuWvZFSC2u'),
 (9, 81444, 'Студент 4', 1, 'student4', '$2y$10$G.hrMEkSs2X.rSesANPQoeJmuh7sHZVdIknMCWDgQVpmqCJ01p8nq'),
-(10, 81555, 'st 5', 1, 'student5', '$2y$10$Qi/BurBzNr60JvlxQxP.s.sCsNm.8ltLtcAXcEIZDn78Yuh88t2g6'),
-(11, 81666, 'st 6', 1, 'student6', '$2y$10$GQY4h5qz/.uEc4EuOdNfIez39TXQsz.hyyjZW5xVdzdm9zQAfIKym'),
-(12, 81777, 'Милена Веселинова Христова', 1, 'student7', '$2y$10$w0pc82Ie7nLaGcvicX7P.e3KZkCrudquu4I1hTHI.frrSJFWaaWz2'),
-(13, 81888, 'st 8', 1, 'student8', '$2y$10$vNBDgV9xNQPJxMYtSok.xeca.yvKLxFfJQ/kREbX.cwOKXswApFYS'),
-(14, 81999, 'st 9', 1, 'student9', '$2y$10$gtTnj8/pcQVUi9UWY9l/Ouv7J3qim4NHwtsFHkYBk1xV2ibQZR8ie'),
-(15, 88111, 'st 10', 1, 'studen10', '$2y$10$Qtf1/6UkSqPbDiHjGHDoX.2/UN.Be9RD9sG.X2rWBwftDsXtonrZu'),
-(16, 88222, 'st 11', 1, 'student11', '$2y$10$sOgMcp7Dr1NmARneIgjH2eRWb.FOCOnQ4EgStCkOQ0IBOm6./xb/S'),
-(17, 88333, 'st 12', 1, 'student12', '$2y$10$d9oIvQc8rWbYBLFjRbV9EeYKQGrsdUr42qKroHYRsIhsJrVLNnplS'),
-(18, 88444, 'st 13', 1, 'student13', '$2y$10$gNc4eGU7OZWmxoBIE34kjO8NBu9yvL4U5qhRzVJDA3B6FKtNtkz9m'),
-(19, 88555, 'st 14', 1, 'student14', '$2y$10$R7k2WkqFDqitOvrSyq08IOwcszcl9z8QNWcD8JeD/kPR7UYssVTn6');
+(10, 81555, 'Студент 5', 1, 'student5', '$2y$10$Qi/BurBzNr60JvlxQxP.s.sCsNm.8ltLtcAXcEIZDn78Yuh88t2g6'),
+(11, 81666, 'Студент 6', 1, 'student6', '$2y$10$GQY4h5qz/.uEc4EuOdNfIez39TXQsz.hyyjZW5xVdzdm9zQAfIKym'),
+(12, 81777, 'Студент 7', 1, 'student7', '$2y$10$w0pc82Ie7nLaGcvicX7P.e3KZkCrudquu4I1hTHI.frrSJFWaaWz2'),
+(13, 81888, 'Студент 8', 1, 'student8', '$2y$10$vNBDgV9xNQPJxMYtSok.xeca.yvKLxFfJQ/kREbX.cwOKXswApFYS'),
+(14, 81999, 'Студент 9', 1, 'student9', '$2y$10$gtTnj8/pcQVUi9UWY9l/Ouv7J3qim4NHwtsFHkYBk1xV2ibQZR8ie'),
+(15, 88111, 'Студент 10', 1, 'studen10', '$2y$10$Qtf1/6UkSqPbDiHjGHDoX.2/UN.Be9RD9sG.X2rWBwftDsXtonrZu'),
+(16, 88222, 'Студент 11', 1, 'student11', '$2y$10$sOgMcp7Dr1NmARneIgjH2eRWb.FOCOnQ4EgStCkOQ0IBOm6./xb/S'),
+(17, 88333, 'Студент 12', 1, 'student12', '$2y$10$d9oIvQc8rWbYBLFjRbV9EeYKQGrsdUr42qKroHYRsIhsJrVLNnplS'),
+(18, 88444, 'Студент 13', 1, 'student13', '$2y$10$gNc4eGU7OZWmxoBIE34kjO8NBu9yvL4U5qhRzVJDA3B6FKtNtkz9m'),
+(19, 88555, 'Студент 14', 1, 'student14', '$2y$10$R7k2WkqFDqitOvrSyq08IOwcszcl9z8QNWcD8JeD/kPR7UYssVTn6'),
+(20, 0, 'Преподавател 2', 2, 'teacher2', '$2y$10$vJlfdGZoaFWjrypD6Wmdhehd2Ug0UuwFU.O7QTM/enQ7teIAUDOe2'),
+(21, 0, 'Преподавател 1', 2, 'teacher1', '$2y$10$wXYUbNojlTFDEQzYsJXNgu.JcmLA8e4XiyPitjAhO0pfmtViL1osG');
 
 --
 -- Indexes for dumped tables
@@ -219,19 +220,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
 
 --
 -- AUTO_INCREMENT for table `room_student`
 --
 ALTER TABLE `room_student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 
 --
 -- AUTO_INCREMENT for table `room_type`
@@ -243,7 +244,7 @@ ALTER TABLE `room_type`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
